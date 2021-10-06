5300f8d7c4969864dfe1a2c75737ba6a78b63f5e
NahuelAbadia <abadianahuel@hotmail.com.ar>
2021-10-06 20:49:18 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Container, Box, Icon, Input, Image, Stack, Text, StackDivider, } from '@chakra-ui/react'
+import { AiFillHome, AiOutlineSearch } from 'react-icons/ai'
+import logo from '../assets/logo.png'
+import { MdHomeFilled, MdOutlineExplore } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsHeart } from 'react-icons/bs'
+import { FaRegUserCircle } from 'react-icons/fa'
+
+const Navbar = () => {
+    return (
+        <Stack>
+            <Container alignSelf="center" maxWidth="container.lg" paddingX={10}>
+                {/* <Container alignSelf="center" maxWidth="container.lg" paddingX={10}> */}
+                    <Stack direction="row" pt={3} alignItems="center" justify="space-between" boxShadow="xs">
+                        <Image src={logo} />
+                        <Stack alignItems="center" direction="row" height="28px" maxWidth="215px" width="100%"
+                            backgroundColor="white" padding={2} borderRadius="xs" boxShadow="xs">
+                            <Icon as={AiOutlineSearch} height={4} width={4} color="gray.400" cursor="pointer"></Icon>
+                            <Input placeholder="Buscar" paddingX={2} variant="unstyled" />
+                        </Stack>
+                        <Stack direction="row" alignItems="flex-start" spacing={4}>
+                            <Icon as={MdHomeFilled} height={7} width={7}></Icon>
+                            <Icon as={RiMessengerLine} height={7} width={7}></Icon>
+                            <Icon as={MdOutlineExplore} height={7} width={7}></Icon>
+                            <Icon as={BsHeart} height={6} width={6}></Icon>
+                            <Icon as={FaRegUserCircle} height={7} width={7}></Icon>
+                        </Stack>
+                    </Stack>
+                {/* </Container> */}
+            </Container>
+        </Stack>
+    )
+}
+
+export default Navbar
